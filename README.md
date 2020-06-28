824fb26ea08e170fb6181bdc43bed73a4b8bd582
A S Md Ferdousul Haque <ferdousul.haque@gmail.com>
2020-06-28 09:17:56 +0600

    Basic Pages Prepare

---
+++ README.md
@@ -0,0 +1,4 @@
+# Order Get Notify
+
+This is a basic application for using firebase messaging if the front order gets an order, notify the delivery.
+
---
+++ lib/main.dart
@@ -0,0 +1,223 @@
+import 'dart:async';
+
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/material.dart';
+
+// Login Page
+class LoginPage extends StatefulWidget {
+  LoginPage({
+    Key key,
+    this.title
+  }): super(key: key);
+
+  // This widget is the login page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State < LoginPage > {
+  TextStyle style = TextStyle(fontFamily: 'Montserrat', fontSize: 20.0);
+
+  @override
+  Widget build(BuildContext context) {
+    final emailField = TextField(
+      obscureText: false,
+      style: style,
+      decoration: InputDecoration(
+        contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+        hintText: "Email",
+        border:
+        OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
+    );
+
+    final passwordField = TextField(
+      obscureText: true,
+      style: style,
+      decoration: InputDecoration(
+        contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+        hintText: "Password",
+        border:
+        OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
+    );
+
+    final loginButton = Material(
+      elevation: 5.0,
+      borderRadius: BorderRadius.circular(30.0),
+      color: Color(0xff01A0C7),
+      child: MaterialButton(
+        minWidth: MediaQuery.of(context).size.width,
+        padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+        onPressed: _showDialog,
+        child: Text("Login",
+          textAlign: TextAlign.center,
+          style: style.copyWith(
+            color: Colors.white, fontWeight: FontWeight.bold)),
+      ),
+    );
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Zappeats Kitchen App'),
+      ),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Container(
+            color: Colors.white,
+            child: Padding(
+              padding: const EdgeInsets.all(36.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: < Widget > [
+                    SizedBox(
+                      height: 155.0,
+                      child: Image.asset(
+                        "assets/c-logo.png",
+                        fit: BoxFit.contain,
+                      ),
+                    ),
+                    SizedBox(height: 45.0),
+                    emailField,
+                    SizedBox(height: 25.0),
+                    passwordField,
+                    SizedBox(
+                      height: 35.0,
+                    ),
+                    loginButton,
+                    SizedBox(
+                      height: 15.0,
+                    ),
+                  ],
+                ),
+            ),
+          ),
+        ),
+      )
+    );
+  }
+
+  // user defined function
+  void _showDialog() {
+    // flutter defined function
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        // return object of type Dialog
+        return AlertDialog(
+          title: new Text("Login Success"),
+          content: new Text("You are now logged in to Zappeats Kitchen"),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(20.0))
+          ),
+          actions: < Widget > [
+            // usually buttons at the bottom of the dialog
+            new FlatButton(
+              child: new Text("Proceed"),
+              onPressed: () {
+                Navigator.of(context).pop();
+                navigateToSubPage(context);
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  Future navigateToSubPage(context) async {
+    Navigator.push(context, MaterialPageRoute(builder: (context) => OrderGetPage()));
+  }
+}
+
+// Order Page
+class OrderGetPage extends StatefulWidget {
+  @override
+  _OrderGetPageState createState() => _OrderGetPageState();
+}
+
+class _OrderGetPageState extends State < OrderGetPage > {
+  String _homeScreenText = "Waiting for token...";
+
+  final FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
+
+  @override
+  void initState() {
+    super.initState();
+    _firebaseMessaging.configure(
+      onMessage: (Map < String, dynamic > message) async {
+        print("onMessage: $message");
+      },
+      onLaunch: (Map < String, dynamic > message) async {
+        print("onLaunch: $message");
+      },
+      onResume: (Map < String, dynamic > message) async {
+        print("onResume: $message");
+      },
+    );
+    _firebaseMessaging.requestNotificationPermissions(
+      const IosNotificationSettings(
+        sound: true, badge: true, alert: true, provisional: true));
+    _firebaseMessaging.onIosSettingsRegistered
+      .listen((IosNotificationSettings settings) {
+        print("Settings registered: $settings");
+      });
+    _firebaseMessaging.getToken().then((String token) {
+      assert(token != null);
+      setState(() {
+        _homeScreenText = "Push Messaging token: $token";
+      });
+      print(_homeScreenText);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Push Messaging Demo'),
+      ),
+      body: Material(
+        child: Column(
+          children: < Widget > [
+            Center(
+              child: Text(_homeScreenText),
+            ),
+
+          ],
+        ),
+      ));
+  }
+
+}
+
+// Home Page of MainApp
+class MainApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: true,
+      title: 'Zappeats Restaurant',
+      theme: ThemeData(
+        primarySwatch: Colors.grey,
+      ),
+      home: LoginPage(title: 'Zappeats Restaurant Login'),
+    );
+  }
+}
+
+void main() {
+  runApp(
+    MainApp()
+  );
+}
