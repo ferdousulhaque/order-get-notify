70343b066e751bd3dbc19e9a56b7eb3a96691203
A S Md Ferdousul Haque <ferdousul.haque@gmail.com>
2020-07-04 08:20:40 +0600

    Moving the Order Placement to Notification Class

--- web/order-placement.php
+++ web/order-placement.php
@@ -23,57 +23,17 @@ $message = $_POST["message"];
         }
         else
         {
-            // display the output
-            echo "<p>";
-            $url = 'https://fcm.googleapis.com/fcm/send';
-
-            $fields = array (
-                    'to' => str_replace(array("\r", "\n"), '',file_get_contents("token.txt")),
-                    'collapse_key' => "type_a",
-                    'data' => array (
-                        "body" => $message,
-                        "description" => $message,
-                        "title" => $message,
-                        "orderId" => 123,
-                        "foodId" => 443,
-                        "shouldSave" => 1,
-                        "id" => 1234,
-                        "click_action" => 'FLUTTER_NOTIFICATION_CLICK',
-                        "status" => 'done'
-                    ),
-                    'notification' => array(
-                        "body" => $message,
-                        "description" => $message,
-                        "title" => $message,
-                        "orderId" => 123,
-                        "foodId" => 443,
-                        "shouldSave" => 1,
-                        "id" => 1234,
-                        "click_action" => 'FLUTTER_NOTIFICATION_CLICK'
-                    )
-            );
-
-            $fields = json_encode ( $fields );
-        
-            $headers = array (
-                    'Authorization: key=' . "AAAAxAr10vg:APA91bGuiArULRZzbGntMfo7esWZcF5wyksp5DfWy5mW4ouYIggOa5dxxkGS8vqI_54k_bCbA0-54C7HoMKdU4VRaypaU9G1qEydqySxQkkgJY4Oa-4gFHL1hFrKD_O_38a6B0i7Fwgx",
-                    'Content-Type: application/json'
-            );
-        
-            $ch = curl_init ();
-            curl_setopt ( $ch, CURLOPT_URL, $url );
-            curl_setopt ( $ch, CURLOPT_POST, true );
-            curl_setopt ( $ch, CURLOPT_HTTPHEADER, $headers );
-            curl_setopt ( $ch, CURLOPT_RETURNTRANSFER, true );
-            curl_setopt ( $ch, CURLOPT_POSTFIELDS, $fields );
-        
-            $result = curl_exec ( $ch );
-            //echo $result;
-            curl_close ( $ch );
-            echo "Message Sent";
-            echo "</p>";
+            include 'notification.php';
+            $device_token = str_replace(array("\r", "\n"), '',file_get_contents("token.txt"));
+            $detail = [
+                "orderId" => 123,
+                "foodId" => 443,
+                "shouldSave" => 1,
+                "id" => 1234
+            ];
+            $notify = new Notification($device_token,$detail);
+            $notify->callNotification();
         }
-        
     ?>
 
     </body>
